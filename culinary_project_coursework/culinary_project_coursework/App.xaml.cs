fe01e58351abbeb1e83822ae91ec2af5c7928847
sonafabristova <sonafabristova@gmail.com>
2025-11-10 09:43:58 +0700

    Add files via upload

---
+++ culinary_project_coursework/culinary_project_coursework/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace culinary_project_coursework
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
